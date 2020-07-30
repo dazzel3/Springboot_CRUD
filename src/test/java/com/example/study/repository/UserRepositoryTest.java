4e8899c33173d28a475591771855f22a626e8f38
dazzel3 <aakk9350@naver.com>
2020-07-31 02:12:56 +0900

    [JPA를 통한 CRUD 학습] Entity 생성과 Repository ... + CRUD 메서드 구현

---
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -0,0 +1,80 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplicationTests;
+import com.example.study.model.entity.User;
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public class UserRepositoryTest extends StudyApplicationTests {
+
+    // Dependency Injection (DI)
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void create() {
+        User user = new User();
+        user.setAccount("TestUser01");
+        user.setEmail("TestUser01@gmail.com");
+        user.setPhoneNumber("010-1234-5678");
+        user.setCreatedAt(LocalDateTime.now());
+        user.setCreatedBy("admin");
+
+        User newUser = userRepository.save(user);
+        System.out.println("newUser: "+newUser);
+    }
+
+    @Test
+    public void read() {
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser -> {
+            System.out.println("user: "+selectUser);
+            System.out.println("email: "+selectUser.getEmail());
+        });
+    }
+
+    @Test
+    public void update() {
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser -> {
+            selectUser.setAccount("pppp");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+        });
+    }
+
+    @Test
+    @Transactional
+    public void delete() {
+        Optional<User> user = userRepository.findById(2L);
+
+        Assert.assertTrue(user.isPresent()); // true
+
+        user.ifPresent(selectUser -> {
+            userRepository.delete(selectUser);
+        });
+
+        Optional<User> deleteUser = userRepository.findById(2L);
+
+        Assert.assertFalse(deleteUser.ifPresent()); // false
+
+//        if(deleteUser.isPresent()) {
+//            System.out.println("데이터 존재 : " +deleteUser.get());
+//        } else {
+//            System.out.println("데이터 삭제 데이터 없음");
+//        }
+
+    }
+
+}
